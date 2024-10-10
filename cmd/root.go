2dbf67d8fdaa124b97fa09371d76010dc7fe6494
Kia Raad <kia.panahirad@gmail.com>
2024-10-11 00:59:19 +0330

    Add root command and configuration initialization
    This commit adds the root command and configuration initializationto the
    codebase. The root command represents the base command when called
    without any subcommands. It also sets up the configuration file and
    environment variable reading. This allows the application to read in the
    config file and environment variables if set.

---
+++ cmd/root.go
@@ -0,0 +1,58 @@
+package cmd
+
+import (
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
+)
+
+var cfgFile string
+
+// rootCmd represents the base command when called without any subcommands
+var rootCmd = &cobra.Command{
+	Use:   "gooler",
+	Short: "A simple CLI tool to generate random UDP packets",
+	Long:  `A tool inspired by Namizum project to generate random UDP packets and keep a 1:10 ration of download/upload for a given NIC.`,
+	// Uncomment the following line if your bare application
+	// has an action associated with it:
+	// Run: func(cmd *cobra.Command, args []string) {
+	// 	log.Default().Println("Gooler!")
+	// },
+}
+
+// Execute adds all child commands to the root command and sets flags appropriately.
+// This is called by main.main(). It only needs to happen once to the rootCmd.
+func Execute() {
+	err := rootCmd.Execute()
+	if err != nil {
+		os.Exit(1)
+	}
+}
+
+func init() {
+	cobra.OnInitialize(initConfig)
+
+	// Here you will define your flags and configuration settings.
+	// Cobra supports persistent flags, which, if defined here,
+	// will be global for your application.
+
+	rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "config", "The config file")
+	rootCmd.MarkPersistentFlagRequired("config")
+}
+
+// initConfig reads in config file and ENV variables if set.
+func initConfig() {
+	viper.SetConfigFile(cfgFile)
+
+	viper.AutomaticEnv() // read in environment variables that match
+
+	// If a config file is found, read it in.
+	if err := viper.ReadInConfig(); err == nil {
+		fmt.Fprintln(os.Stderr, "Using config file:", viper.ConfigFileUsed())
+	} else {
+		log.Fatalf("Error reading config file: %s", cfgFile)
+	}
+}
---
+++ main.go
@@ -0,0 +1,10 @@
+/*
+Copyright © 2024 Kia Raad <kia.panahirad@gmail.com>
+*/
+package main
+
+import "github.com/kiapanahi/gooler/cmd"
+
+func main() {
+	cmd.Execute()
+}
