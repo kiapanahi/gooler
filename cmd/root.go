55cf8349ecaa69bda26fb912e1cee2003d7c74b3
Kia Raad <kia.panahirad@gmail.com>
2024-10-11 21:37:10 +0330

    Refactor root command initialization and configuration handling

--- cmd/root.go
+++ cmd/root.go
@@ -1,7 +1,6 @@
 package cmd
 
 import (
-	"fmt"
 	"log"
 	"os"
 
@@ -35,10 +34,6 @@ func Execute() {
 func init() {
 	cobra.OnInitialize(initConfig)
 
-	// Here you will define your flags and configuration settings.
-	// Cobra supports persistent flags, which, if defined here,
-	// will be global for your application.
-
 	rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "config", "The config file")
 	rootCmd.MarkPersistentFlagRequired("config")
 }
@@ -50,8 +45,9 @@ func initConfig() {
 	viper.AutomaticEnv() // read in environment variables that match
 
 	// If a config file is found, read it in.
+
 	if err := viper.ReadInConfig(); err == nil {
-		fmt.Fprintln(os.Stderr, "Using config file:", viper.ConfigFileUsed())
+		log.Default().Println("Using config file:", viper.ConfigFileUsed())
 	} else {
 		log.Fatalf("Error reading config file: %s", cfgFile)
 	}
